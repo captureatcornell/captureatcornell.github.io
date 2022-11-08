b47e09592fd1e1e583606edfd76a7fa02913245a
Ming DeMers <mtd64@cornell.edu>
2022-11-08 13:29:31 -0500

    Updated to Card-based visual style.
    - E-board now are cards that are interactive
    - Visual theme now cards
    - Bug fixes (links dissapearing, links don't open a new tab)

---
+++ scripts/eboard-gallery.js
@@ -0,0 +1,67 @@
+$(document).ready(function () {
+  $('.card-back').addClass('hidden');
+});
+
+$('#demers').click(function () {
+  $('#demers').toggleClass('flip-card');
+  setTimeout(function () {
+    $('#demers .card-front').toggleClass('hidden');
+    $('#demers .card-back').toggleClass('hidden');
+  }, 300);
+});
+
+$('#curtis').click(function () {
+  $('#curtis').toggleClass('flip-card');
+  setTimeout(function () {
+    $('#curtis .card-front').toggleClass('hidden');
+    $('#curtis .card-back').toggleClass('hidden');
+  }, 300);
+});
+
+$('#dunn').click(function () {
+  $('#dunn').toggleClass('flip-card');
+  setTimeout(function () {
+    $('#dunn .card-front').toggleClass('hidden');
+    $('#dunn .card-back').toggleClass('hidden');
+  }, 300);
+});
+
+$('#gluza').click(function () {
+  $('#gluza').toggleClass('flip-card');
+  setTimeout(function () {
+    $('#gluza .card-front').toggleClass('hidden');
+    $('#gluza .card-back').toggleClass('hidden');
+  }, 300);
+});
+
+$('#newell').click(function () {
+  $('#newell').toggleClass('flip-card');
+  setTimeout(function () {
+    $('#newell .card-front').toggleClass('hidden');
+    $('#newell .card-back').toggleClass('hidden');
+  }, 300);
+});
+
+$('#cao').click(function () {
+  $('#cao').toggleClass('flip-card');
+  setTimeout(function () {
+    $('#cao .card-front').toggleClass('hidden');
+    $('#cao .card-back').toggleClass('hidden');
+  }, 300);
+});
+
+$('#zhang').click(function () {
+  $('#zhang').toggleClass('flip-card');
+  setTimeout(function () {
+    $('#zhang .card-front').toggleClass('hidden');
+    $('#zhang .card-back').toggleClass('hidden');
+  }, 300);
+});
+
+$('#goldstein').click(function () {
+  $('#goldstein').toggleClass('flip-card');
+  setTimeout(function () {
+    $('#goldstein .card-front').toggleClass('hidden');
+    $('#goldstein .card-back').toggleClass('hidden');
+  }, 300);
+});
